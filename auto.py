d3903c60845256152d6bd5614499c45bff24eaca
Mohamed Adel <54896810+adel0019@users.noreply.github.com>
2023-08-13 14:55:14 -0400

    added in a check for similar version numbers in installations folder

--- auto.py
+++ auto.py
@@ -6,6 +6,7 @@ import zipfile
 import os
 import logging
 import subprocess
+import re
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -16,15 +17,13 @@ dest_dir_documents = "/Users/Mohamed/OneDrive/Documents/Downloaded Docs"
 dest_dir_installations = "/Users/Mohamed/Onedrive/Installations"
 dest_dir_zipfiles = "/Users/Mohamed/Onedrive/Extracted Zip Files"
 
-#  supported image types
+# Supported image types
 image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd",
-                    ".raw", ".arw", ".cr2", ".nrw",
-                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf",
-                    ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+                    ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt",
+                    ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
 
-#  supported Document types
-document_extensions = [".doc", ".docx", ".odt",
-                       ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+# Supported Document types
+document_extensions = [".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
 
 
 class AppUpdater:
@@ -35,13 +34,13 @@ class AppUpdater:
         except subprocess.CalledProcessError as e:
             logging.error(f"Error while updating apps: {e}")
 
+
 def make_unique(dest, name):
     filename, extension = splitext(name)
     counter = 1
     while exists(f"{dest}/{name}"):
         name = f"{filename}({str(counter)}){extension}"
         counter += 1
-
     return name
 
 
@@ -71,20 +70,54 @@ class MoverHandler(FileSystemEventHandler):
             name = os.path.basename(entry)
             self.check_zip_files(entry, name)
 
-# Checks for update files in .msi or .exe format then puts them in Installations folder
     def check_installation_files(self, entry, name):
         if name.endswith('.exe') or name.endswith('.msi'):
             # Check if there's a corresponding .part file
             part_file = entry + ".part"
-
             if os.path.exists(part_file):
                 logging.info(f"Waiting for download to complete: {name}")
                 while os.path.exists(part_file):
-                    sleep(10)  # Adjust the interval as needed
+                    sleep(10)
                     logging.info(f"Still waiting for download: {name}")
+
+            # Check for existing similar files
+            existing_files = os.listdir(dest_dir_installations)
+            for existing_file in existing_files:
+                if self.is_older_version(name, existing_file):
+                    logging.info(f"Skipping older version: {name}")
+                    return  # Don't move if an older version exists
 
             move_file(dest_dir_installations, entry, name)
             logging.info(f"Moved installation file: {name}")
+
+    def is_older_version(self, new_name, existing_name):
+        new_version = self.extract_version(new_name)
+        existing_version = self.extract_version(existing_name)
+
+        if new_version and existing_version:
+            return self.compare_versions(new_version, existing_version)
+        else:
+            return False
+
+    def extract_version(self, filename):
+        version_pattern = r"-(\d+(\.\d+)*)\.(exe|msi)$"
+        match = re.search(version_pattern, filename)
+        if match:
+            return match.group(1)
+        else:
+            return None
+
+    def compare_versions(self, new_version, existing_version):
+        new_parts = [int(part) for part in new_version.split('.')]
+        existing_parts = [int(part) for part in existing_version.split('.')]
+
+        for new_part, existing_part in zip(new_parts, existing_parts):
+            if new_part > existing_part:
+                return False
+            elif new_part < existing_part:
+                return True
+
+        return False
 
     def check_image_files(self, entry, name):
         for image_extension in image_extensions:
@@ -98,12 +131,9 @@ class MoverHandler(FileSystemEventHandler):
                 move_file(dest_dir_documents, entry, name)
                 logging.info(f"Moved document file: {name}")
 
-# Checks for zip files, extracts them, puts them into a folder then deletes the empty zip file
     def check_zip_files(self, entry, name):
-        print(f"Checking zip file: {name}")
         if name.lower().endswith('.zip'):
             try:
-                print(f"Processing zip file: {name}")
                 with zipfile.ZipFile(entry, 'r') as zip_ref:
                     zip_ref.extractall(dest_dir_zipfiles)
                 os.remove(entry)
@@ -126,8 +156,7 @@ if __name__ == "__main__":
 
     try:
         while True:
-            # Check for updates once a week
-            sleep_duration = 604800
+            sleep_duration = 604800  # Check for updates once a week
             sleep(sleep_duration)
             app_updater.update_apps()
     except KeyboardInterrupt:
